5b574a20821253a51c919f7210426b8fd2a8b188
Ethan-Edmond <eedmond@uccs.edu>
2021-05-04 13:28:42 -0600

    Finished a couple of the event types

--- js/index.js
+++ js/index.js
@@ -25,3 +25,41 @@ links.forEach(function(link){
     document.body.style.backgroundColor = `hsla(${150 + ((scrollY * 60) / document.body.scrollHeight)}, 100%, 50%, ${Math.sin((scrollY * Math.PI) / document.body.scrollHeight)})`;
   });
 }
+{
+  const headlines = document.querySelectorAll("h2");
+  let bigWords = false;
+  function setTransitionNdoublebump(header){
+    header.style.transition = "font-size 1s";
+    header.addEventListener("dblclick", (event)=> {
+      if (!bigWords){
+        header.style.fontSize = "4.3rem";
+      } else {
+        header.style.fontSize = "3.2rem";
+      }
+      bigWords = !bigWords;
+    });
+  }
+  headlines.forEach(setTransitionNdoublebump);
+}
+function selectRandomize(event){
+  let alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
+  let tempString = "";
+  for(let i = 0; i < 10; i++){
+    tempString += alphabet[Math.floor(Math.random() * 26)];
+  }
+  event.target.value = tempString;
+  event.stopPropagation();
+}
+let first = document.createElement("input");
+first.style.type = "text";
+first.value = "asdfadf";
+document.body.append(first);
+first.addEventListener("select", selectRandomize);
+
+// this is so horrible
+function fontColorRand(event){
+  if (event.keyCode === 32){
+    document.body.style.color = `#${Math.floor(Math.random() * (256**3)).toString(16)}`;
+  }
+}
+document.addEventListener("keydown", fontColorRand);
